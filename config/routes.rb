b5ecace3f81d56c3f6b0de026f775f87fc2aa8cc
Jennifer Choi <fashed@gmail.com>
2016-03-18 17:58:49 -0400

    toastr notifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'pages/about'
-  root to: "home#index"
+  root to: "pages#about"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
