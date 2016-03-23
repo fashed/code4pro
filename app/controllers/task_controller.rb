b8c2991c37215feb112f9b9feba7ce337e1e8fa1
Jennifer Choi <fashed@gmail.com>
2016-03-23 00:34:42 -0400

    Update task controller to only allow logged in users

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,10 +1,25 @@
 class TaskController < ApplicationController
+
+	before_action :authenticate_user!
+
   def show
   	project = Project.find(params[:project_id])
   	@tasks = project.tasks.order(:tag)
-  	@task = @tasks.find(params[:id])
 
-  	@next_task = @task.next
-  	@prev_task = @task.prev
+  	joined = false
+
+  	if !current_user.nil? && !current_user.projects.nil?
+  		#check if @project is already inside current_user's project. if current user already joined project
+  		joined = current_user.projects.include?(@project)
+  	end
+
+  	if joined
+  	  @task = @tasks.find(params[:id])
+	  	@next_task = @task.next
+	  	@prev_task = @task.prev
+	  else
+	  	flash[:notice] = "No permission to view this. Please log in."
+	  	redirect_to project
+	  end
   end
 end
