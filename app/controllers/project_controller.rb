a1a04b5a417e32808195edbb260960a36bd7dea4
Jennifer Choi <fashed@gmail.com>
2016-03-22 22:36:57 -0400

    Updated Project Controller and show. Allow users to take project when logged in.

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -6,6 +6,15 @@ class ProjectController < ApplicationController
   def show
   	@project = Project.find(params[:id])
   	@tasks = @project.tasks.order(:tag)
+
+  	@joined = false
+
+  	if !current_user.nil? && !current_user.projects.nil?
+  		#check if @project is already inside current_user's project. if current user already joined project
+  		@joined = current_user.projects.include?(@project)
+  	end
+
+  	@users = @project.users.order('created_at desc').first(10)
   end
   
 end
